8ddb5f37e457ec7af94cd172f4a32e278b65c833
rachelgrau <rgrau@stanford.edu>
2015-11-01 23:51:02 -0800

    Initial Commit

---
+++ LoopedIn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoopedIn
+//
+//  Created by Rachel on 11/1/15.
+//  Copyright (c) 2015 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

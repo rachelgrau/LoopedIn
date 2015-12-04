d6dd1874cc51625cb4afdd4383e702759051c3ab
rachelgrau <rgrau@stanford.edu>
2015-12-04 02:17:05 -0800

    Before refactoring student-class-task vc

---
+++ LoopedIn/SingleClassTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  SingleClassTaskViewController.h
+//  LoopedIn
+//
+//  Created by Rachel on 12/4/15.
+//  Copyright © 2015 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleClassTaskViewController : UIViewController
+
+@end
---
+++ LoopedIn/SingleClassTaskViewController.m
@@ -0,0 +1,37 @@
+//
+//  SingleClassTaskViewController.m
+//  LoopedIn
+//
+//  Created by Rachel on 12/4/15.
+//  Copyright © 2015 Rachel. All rights reserved.
+//
+
+#import "SingleClassTaskViewController.h"
+
+@interface SingleClassTaskViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation SingleClassTaskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
